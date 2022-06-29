45df1e938a60ebee12f0575616f6047222f57117
Helena Oliveira <helena@informach.com.br>
2022-06-29 13:52:19 -0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Event } from "./pages/Event"
+import { Router } from "./components/Router"
+import { ApolloProvider } from '@apollo/client'
+import { client } from './lib/apollo'
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   )
 }
